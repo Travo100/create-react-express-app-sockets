411a9c54e5ad41b9f7275fb1aa76e04f4e7f4616
Travo100 <Travo100@gmail.com>
2018-05-21 14:52:55 -0700

    Added a better check for sockets urls

--- client/src/utils/sockets.js
+++ client/src/utils/sockets.js
@@ -1,8 +1,5 @@
 import openSocket from 'socket.io-client';
-// comment this out for local
-//const SOCKETS_URL = 'https://create-react-express-sockets.herokuapp.com/';
-// comment this out to work locally
-const SOCKETS_URL = 'http://localhost:3001';
+const SOCKETS_URL = process.env.SOCKETS_URL || 'http://localhost:3001';
 const socket = openSocket(SOCKETS_URL);
 
 const sockets = {
