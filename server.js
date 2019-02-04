9037bbf6ff4068f2035ad48a87e8299bbec637b3
Travo100 <travo100@gmail.com>
2019-02-04 08:12:05 -0800

    Server

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const logger = require("morgan");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
